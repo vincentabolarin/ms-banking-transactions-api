a173e9e911867de56dcafbd47b700ee5bfd1d0cf
Vincent Abolarin <80973232+vincentabolarin@users.noreply.github.com>
2025-03-29 23:43:35 +0100

    remove createdAt and updatedAt from Account interface

--- src/models/account.model.ts
+++ src/models/account.model.ts
@@ -4,8 +4,6 @@ interface IAccount extends mongoose.Document {
   userId: mongoose.Schema.Types.ObjectId;
   balance: number;
   currency: string;
-  createdAt: Date;
-  updatedAt: Date;
 }
 
 const AccountSchema = new mongoose.Schema(
