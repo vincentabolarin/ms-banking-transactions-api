db438a3fdb1bc2f598bec9040a6a54496ffdb7da
Vincent Abolarin <80973232+vincentabolarin@users.noreply.github.com>
2025-03-29 23:15:18 +0100

    set up User schema

---
+++ src/models/user.model.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+interface User extends mongoose.Document {
+  email: string;
+  password: string;
+}
+
+const UserSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+export default mongoose.model<User>("User", UserSchema);
