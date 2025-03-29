d98ee5dae2e89279e4bd3b73bf5d93846a0b1ab0
Vincent Abolarin <80973232+vincentabolarin@users.noreply.github.com>
2025-03-29 23:09:56 +0100

    set up Account schema

---
+++ src/models/account.model.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+interface Account extends Document {
+  userId: mongoose.Schema.Types.ObjectId;
+  balance: number;
+  currency: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const AccountSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    balance: { type: Number, required: true, default: 0 },
+    currency: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model<Account>("Account", AccountSchema);
