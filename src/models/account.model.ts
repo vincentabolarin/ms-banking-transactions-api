e3d37335480d778a88ecd1421e1872e7470e9ec4
Vincent Abolarin <80973232+vincentabolarin@users.noreply.github.com>
2025-03-29 23:46:21 +0100

    export Account interface

--- src/models/account.model.ts
+++ src/models/account.model.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-interface IAccount extends mongoose.Document {
+export interface IAccount extends mongoose.Document {
   userId: mongoose.Schema.Types.ObjectId;
   balance: number;
   currency: string;
