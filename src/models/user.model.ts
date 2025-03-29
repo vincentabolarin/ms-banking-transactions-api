b7f224ec020f8ffbd2e907e93c8bd7b298afdec8
Vincent Abolarin <80973232+vincentabolarin@users.noreply.github.com>
2025-03-29 23:42:49 +0100

    add timestamps to User schema

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -8,6 +8,6 @@ export interface IUser extends mongoose.Document {
 const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-});
+}, { timestamps: true });
 
 export default mongoose.model<IUser>("User", UserSchema);
