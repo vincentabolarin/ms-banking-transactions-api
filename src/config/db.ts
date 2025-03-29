1edcda6772c9d587485e52851194cd7c27e5ae48
Vincent Abolarin <80973232+vincentabolarin@users.noreply.github.com>
2025-03-29 22:09:45 +0100

    configure database

---
+++ src/config/db.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI as string);
+    console.log("MongoDB Connected");
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error("MongoDB connection error:", error.message);
+    } else {
+      console.error("An unknown error occurred:", error);
+    }
+    process.exit(1);
+  }
+};
+
+export default connectDB;
