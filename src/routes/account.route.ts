e1b4238f1f4152bc9189d4d8c294f562744c5212
Vincent Abolarin <80973232+vincentabolarin@users.noreply.github.com>
2025-04-01 23:05:31 +0100

    implement input sanitisation and validation

--- src/routes/account.route.ts
+++ src/routes/account.route.ts
@@ -2,11 +2,12 @@ import { Router } from "express";
 import { container } from "tsyringe";
 import { AccountController } from "../controllers/account.controller.js";
 import authMiddleware from "../middlewares/auth.middleware.js";
+import { validateAccountCreation } from "../middlewares/accountValidation.middleware.js";
 
 const router = Router();
 const accountController = container.resolve(AccountController);
 
-router.post("/", authMiddleware, accountController.createAccount);
+router.post("/", validateAccountCreation, authMiddleware, accountController.createAccount);
 router.get("/", authMiddleware, accountController.getAccount);
 
 export default router;
--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { container } from "tsyringe";
 import { AuthController } from "../controllers/auth.controller.js";
+import { validateLogin, validateRegistration } from "../middlewares/userValidation.middleware.js";
 
 const router = Router();
 const authController = container.resolve(AuthController);
 
-router.post("/register", authController.register);
-router.post("/login", authController.login);
+router.post("/register", validateRegistration, authController.register);
+router.post("/login", validateLogin, authController.login);
 
 export default router;
